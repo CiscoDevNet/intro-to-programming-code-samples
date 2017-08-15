d01846b22e5c2b6930e686307ced80c9759d7841
Armen Martirosyan <armartirosyan@gmail.com>
2017-08-15 07:29:57 -0700

    adding code samples

---
+++ intro-to-python-parsing-json/json_parse_1.py
@@ -0,0 +1,32 @@
+# Part 1
+var={"car":"volvo", "fruit":"apple"}
+print(var["fruit"])
+for f in var:	
+	print("key: " + f + " value: " + var[f])
+
+# Printing blank lines
+print()
+print()
+
+# Part 2	
+var1={"donut":["chocolate","glazed","sprinkled"]}
+print(var1["donut"][0])
+print("My favorite donut flavors are:", end= " ")
+for f in var1["donut"]:
+	print(f)
+
+# Printing blank lines
+print()
+print()
+
+#Using the examples above write code to print one value of each JSON structure and a loop to print all values.	
+var={"vegetable":"carrot", "fruit":"apple","animal":"cat","day":"Friday"}
+
+
+var1={"animal":["dog","cat","fish","tiger","camel"]}
+
+
+myvar={"dessert":"ice cream", "exercise":"push ups","eyes":"blue","gender":"male"}
+	
+
+myvar1={"dessert":["cake","candy","ice cream","pudding","cookies"]}
